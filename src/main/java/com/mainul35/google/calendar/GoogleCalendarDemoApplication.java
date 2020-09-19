56a920cae0d14957dcd93cf6b842e0233f5b5c82
mainul35-bs23 <mainul.hasan@bs-23.net>
2020-09-19 11:36:56 +0600

    initial commit

---
+++ src/main/java/com/mainul35/google/calendar/GoogleCalendarDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mainul35.google.calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCalendarDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleCalendarDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mainul35/google/calendar/GoogleCalendarDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainul35.google.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleCalendarDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
